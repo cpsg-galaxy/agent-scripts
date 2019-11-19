947a8bf1496394f963d01c949d6e845f824654ee
anhorrig <56142654+anhorrig@users.noreply.github.com>
2019-11-18 17:17:03 -0800

    Update appd-php_install-script.sh

--- appd-php_install-script.sh
+++ appd-php_install-script.sh
@@ -10,33 +10,32 @@ agentSendLogMessage "Installing AppDynamics Agent..."
 set -e
 set -o pipefail
 
-# Add Host file entry for appd-controller
+#Set variables
+tiername=$5"_"$6
 
+#Add Host file entry for appd-controller
 sudo echo "$2	appd-controller" | sudo tee --append /etc/hosts > /dev/null
 
 #Create installation directory and move agent
-
-cd /var/tmp/
-mkdir appd-php
-cd /var/tmp/appd-php
-mv /var/tmp/$1 .
+mkdir -p /opt/appdynamics/appd-php
+cd /opt/appdynamics/appd-php
+mv /tmp/$1 .
 
 #Unzip the AppD Agent
-
 tar -xvjf $1
-
 cd appdynamics-php-agent-linux_x64
-chmod 777 logs/
-tiername=$5"_"$6
-sudo ./install.sh -a=$3  $2 8090 $4 $tiername $7
+
+#Change permissions for the PHP and logs folders
+chmod -R 777 /opt/appdynamics/appd-php
+chmod -R 777 /opt/appdynamics/appd-php/logs
+
+#Install the appd-php agent
+./install.sh -a=$3  $2 8090 $4 $tiername $7
 
 # Restart apache httpd
-
 sudo service httpd restart
 
-# Clean up the temporary file and the agent package file
-
-rm -rf /tmp/appd-php/downloadagent.done
-rm -rf /tmp/appd-php/appdynamics-php-agent-x64-linux-4.3.5.0.tar.bz2
+# Clean up the agent package file
+rm -rf $1
 
 agentSendLogMessage "Installing AppDynamics Agent...done"
